bd505d531b046033e0e97f01542f0b6601a0f50b
Alexandre Leroux <alex@sherpa.one>
2023-03-06 18:53:36 +0100

    up

---
+++ README.md
@@ -0,0 +1,16 @@
+# Todo List
+
+Application de type Todo List développée en JavaScript ES6+ (Vanilla).
+
+--
+
+<img src="https://sherpa.one/images/sherpa-logotype.png" width="120px">
+
+__Alexandre Leroux__
+
+_Enseignant / Formateur_<br>
+_Développeur logiciel web & mobile_
+
+Nancy (Grand Est, France)
+
+https://sherpa.one
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 //écoute de l'événement "DOMContentLoaded" et exécution de la fonction init lorsque le DOM de la page HTML est chargé.
 document.addEventListener("DOMContentLoaded", init);
 
-//https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+//créé une classe Task
+class Task {
+    constructor(content, done = false, important = false) {
+        this.content = content;
+        this.done = done;
+        this.important = important;
+        this.created_at = new Date();
+    }
+}
 
-const planets = [
-    { name: "Mercure", mass: 0.330, diameter: 4879, density: 5429, number_of_moons: 0, distance_from_sun: 57.9 },
-    { name: "Vénus", mass: 4.87, diameter: 12.104, density: 5243, number_of_moons: 0, distance_from_sun: 0 },
-    { name: "Terre", mass: 5.97, diameter: 12.756, density: 5514, number_of_moons: 1, distance_from_sun: 149.6 },
-    { name: "Mars", mass: 0, diameter: 0, density: 0, number_of_moons: 0, distance_from_sun: 0 },
-    { name: "Jupiter", mass: 0, diameter: 0, density: 0, number_of_moons: 0, distance_from_sun: 0 },
-    { name: "Saturne", mass: 0, diameter: 0, density: 0, number_of_moons: 0, distance_from_sun: 0 },
-    { name: "Uranus", mass: 0, diameter: 0, density: 0, number_of_moons: 0, distance_from_sun: 0 },
-    { name: "Neptune", mass: 0, diameter: 0, density: 0, number_of_moons: 0, distance_from_sun: 0 },
+//initialise une collection d'objets de type Task
+const tasks = [
+    new Task("Acheter des fleurs"),
+    new Task("Aller à la pharmacie", true),
+    new Task("Sortir le chien", false, true),
+    new Task("Prendre rdv chez le médecin"),
+    new Task("Réserver hotel pour vacances d'été"),
+    new Task("Déclaration d'impôts"),
+    new Task("Acheter cadeau pour anniversaire de Jean", true)
 ];
-
 
 function onClick(event) {
     console.log(event);
@@ -21,43 +28,121 @@ function onClick(event) {
 
 function onSubmit(event) {
     event.preventDefault();//évite le rechargement de page
+    const content = document.querySelector('#task_content').value;
 
-
-    const keyword_input = document.querySelector("#keyword");
-
-    const keyword = keyword_input.value;//valeur du champ keyword
-
-    let found_planet;
-
-    for (const a_planet of planets) {
-        if (a_planet.name.toLowerCase() === keyword.toLowerCase()) {
-            found_planet = a_planet;
-        }
+    if (content === "") {
+        alert("Vous devez saisir un contenu");
+        return;
     }
 
-    if (found_planet) {
-        const search_results = document.querySelector("#search-results");
+    const task = new Task(content);
 
-        const template = `
-            <h2>${found_planet.name}</h2>
-            <p>Masse : ${found_planet.mass}10<sup>24</sup>kg</p>
-        `;
+    tasks.unshift(task);
 
-        search_results.innerHTML = template;
-        console.log("La planète existe");
-    } else {
-        console.log("La planète n'existe pas");
+    refresh_tasks();
+}
+
+function refresh_tasks() {
+    const tasks_container = document.querySelector("#tasks_container");
+
+    let html = "";//par défaut le contenu html de la balise tasks_container est vide 
+
+    tasks.map(a_task => html += task_template(a_task));
+    //pour chaque task contenue dans la liste on ajoute une balise article au contenu html de la balise tasks_container
+
+    tasks_container.innerHTML = html;
+    //on met à jour le contenu html de la balise tasks_container
+
+    const buttons = document.querySelectorAll("article.task button");
+    //on récupère chaque bouton dans une balise article 
+
+    for (const btn of buttons) {
+        //on ajoute un écouteur sur l'événement click de chaque bouton
+        btn.addEventListener('click', (event) => {
+
+            //récupère la balise article parent du bouton cliqué
+            const task = event.currentTarget.parentNode;
+
+            //récupère la valeur de data-status de la balise article (parent du bouton cliqué)
+            const status = task.dataset.status;
+
+            if (status === "todo") {
+                //modifie la valeur de data-status de la balise article à done
+                task.dataset.status = "done";
+
+                //remplace la classe de la balise article de todo à done
+                task.className = task.className.replace("todo", "done");
+
+                //remplace le texte du bouton de V à x
+                task.querySelector("button").innerText = "x";
+            } else {
+                //supprime la balise article du DOM
+                document.querySelector("#tasks_container").removeChild(task);
+            }
+        });
     }
+}
 
+function get_classname_according_done(done) {
+    //retourne "done" ou "todo" en fonction de la valeur de done (true / false)
+    return done ? "done" : "todo";
+}
 
+function get_classname_according_importance(important) {
+    //retourne "important" ou "normal" en fonction de la valeur de important (true / false)
+    return important ? "important" : "normal";
+}
+
+function format_date(date) {
+    //formate la date
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = date.getFullYear();
+
+    return dd + '/' + mm + '/' + yyyy;
+}
+
+function get_label_according_done(done) {
+    //retourne "x" ou "V" selon la valeur de done (true / false)
+    return done ? "x" : "V";
+}
+
+function get_dataStatus_according_done(done) {
+    //retourne "done" ou "todo" selon la valeur de done (true / false)
+    return done ? "done" : "todo";
+}
+
+function task_template(task) {
+    //retourne une balise article dynamiquement complétée selon les valeur de l'objet Task passé en paramètre
+    return `
+    <article class="task
+    ${get_classname_according_done(task.done)}
+    ${get_classname_according_importance(task.important)}
+    "
+    data-status="${get_dataStatus_according_done(task.done)}"
+    >
+        ${task.content}
+
+        <span>
+        (${format_date(task.created_at)})
+        </span>
+        
+        <button class="button button-clear"> 
+        ${get_label_according_done(task.done)}
+        </button>
+    
+    </article>`;
 }
 
 function init() {
+    //initialise le script
     console.log('init');
 
-    const test_button = document.querySelector("#test");
-    test_button.addEventListener('click', onClick);
+    refresh_tasks();
+    //met à jour le contenu de la balise 
 
-    const search_form = document.querySelector("#search-form");
-    search_form.addEventListener('submit', onSubmit);
+    const add_task_form = document.querySelector("#add_task_form");
+
+    add_task_form.addEventListener('submit', onSubmit);
+    //ajoute un écouteur sur l'événement submit du formulaire add_task_form
 }
